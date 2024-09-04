680ff8c08f46959e87162d7560c6b94bae43ace3
drew-codes <hello@drewthompson.dev>
2024-09-03 23:04:18 -0400

    scrape categories

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,17 +4,25 @@ from parser import get_parsed_html
 
 def get_categories(driver, wait):
     # scrape categories:
-    go_to_page("/food/c/27985", driver=driver, wait=wait, wait_element_class="")
+    go_to_page(
+        "/food/c/27985", driver=driver, wait=wait, wait_element_class="link-list-maxH"
+    )
 
     parsed_html = get_parsed_html(driver.page_source)
-    # 1. Go to food page (https://www.loblaws.ca/food/c/27985)
-    # 2. Grab each category name and url
-    # 3. return the category names and urls
-    print(parsed_html)
-    return
+
+    categories_container = parsed_html.find("div", class_="link-list-maxH")
+    category_tuples = []
+
+    for elm in categories_container.select(".chakra-link"):
+        if "See All" in elm.text:
+            continue
+
+        category_tuples.append((elm.text, elm.attrs["href"]))
+
+    return category_tuples
 
 
-def get_sub_categories():
+def get_sub_categories(categories):
     # scrape subcategories:
     # 1. Go to each category
     # 2. From the Sidebar, Get every sub-category name and its corresponding all page url
@@ -30,12 +38,11 @@ def get_products():
 
 
 def main():
-    print("executing...")
     driver, wait = get_driver_with_wait()
     categories = get_categories(driver, wait)
     # save them here once db is ready
 
-    sub_categories = get_sub_categories()
+    sub_categories = get_sub_categories(categories)
     # save them here once db is ready
 
     products = get_products()
