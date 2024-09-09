a0493dad350df61b4d52164e016a120ac6f90e5d
drew-codes <hello@drewthompson.dev>
2024-09-08 23:00:15 -0400

    add postgres, add a few models

---
+++ data_models/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Categories(models.Model):
+
+    name = models.CharField(max_length=100)
+    vendor_url = models.URLField()
+    vendor_id = models.CharField(max_length=10)
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    vendor_url = models.URLField()
+    vendor_id = models.CharField(max_length=15)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    product_description = models.TextField()
+    vendor_id = models.CharField(max_length=15)
+    ingredients = models.ManyToManyField(Ingredient)
--- food_app/settings.py
+++ food_app/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = "food_app.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "food_db_service",
+        },
     }
 }
 
