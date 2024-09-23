934ba507a08c5793841777b24366af0e1e244198
drew-codes <hello@drewthompson.dev>
2024-09-22 23:24:22 -0400

    recursively get product links when sub_cat has pagination

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,4 @@
-from data_models.service import save_categories, save_products, save_sub_categories
+from data_models.service import save_categories, save_sub_categories
 from .driver import get_driver_with_wait, go_to_page_container
 from .parser import get_parsed_html
 from .utils.select_count import select_count
@@ -75,27 +75,67 @@ def get_sub_categories(categories, driver, wait):
     return sub_category_tuples
 
 
-def get_products(sub_categories, driver, wait):
+def has_next_page(parsed_html, current_page):
+    pagination_links = parsed_html.css.select("nav[data-testid='pagination'] a")
+
+    for link in pagination_links:
+        if link.text == str(current_page + 1):
+            return True
+
+    return False
+
+
+def get_product_links(
+    url_path,
+    driver,
+    wait,
+    links=[],
+    current_page=1,
+):
+    PRODUCT_ITEM_SELECTOR = "div[data-testid='product-grid'] a.css-1hnz6hu"
+
+    url_path_with_page = url_path + f"?page={current_page}"
+
+    go_to_page_container(
+        url_path_with_page,
+        driver=driver,
+        wait=wait,
+        wait_css_selector=PRODUCT_ITEM_SELECTOR,
+        retries=2,
+    )
+
+    parsed_html = get_parsed_html(driver.page_source)
+
+    links.extend(parsed_html.css.select(PRODUCT_ITEM_SELECTOR))
+
+    if has_next_page(parsed_html, current_page):
+        return get_product_links(
+            url_path,
+            driver,
+            wait,
+            links,
+            current_page + 1,
+        )
+
+    return links
+
+
+def get_product_name_with_url(sub_categories, driver, wait):
     product_links = []
     for sub_cat in select_count(sub_categories, 1):
         sub_cat_name, url_path = sub_cat.name, sub_cat.vendor_url
-        PRODUCT_ITEM_SELECTOR = "div[data-testid='product-grid'] a.css-1hnz6hu"
 
         try:
-            go_to_page_container(
-                url_path,
-                driver=driver,
-                wait=wait,
-                wait_css_selector=PRODUCT_ITEM_SELECTOR,
-                retries=2,
+            product_links.extend(
+                get_product_links(
+                    url_path,
+                    driver,
+                    wait,
+                )
             )
 
-            parsed_html = get_parsed_html(driver.page_source)
-
-            product_links.extend(parsed_html.css.select(PRODUCT_ITEM_SELECTOR))
-
         except Exception as error:
-            print(f"[Products]: Could not load and/or parse {sub_cat_name} page", error)
+            print(f"[Products]: Could not load and/or parse {sub_cat_name}", error)
             continue
 
     product_names_with_urls = []
@@ -105,6 +145,36 @@ def get_products(sub_categories, driver, wait):
         product_names_with_urls.append((name, link.attrs["href"]))
 
     return product_names_with_urls
+
+
+def get_product_details(products, driver, wait):
+    product_details = []
+
+    for product in select_count(products, 1):
+        product_name, url_path = product.name, product.vendor_url
+        PRODUCT_DETAILS_SELECTOR = "div[data-testid='product-details']"
+
+        try:
+            go_to_page_container(
+                url_path,
+                driver=driver,
+                wait=wait,
+                wait_css_selector=PRODUCT_DETAILS_SELECTOR,
+                retries=2,
+            )
+
+            parsed_html = get_parsed_html(driver.page_source)
+
+            product_details.append(parsed_html.css.select(PRODUCT_DETAILS_SELECTOR))
+
+        except Exception as error:
+            print(
+                f"[Product Details]: Could not load and/or parse {product_name} page",
+                error,
+            )
+            continue
+
+    return product_details
 
 
 def run_scraper():
@@ -126,7 +196,7 @@ def run_scraper():
     print(sub_categories)
     print("====================================")
 
-    products = get_products(
+    products_names_with_url = get_product_name_with_url(
         sub_categories=saved_sub_categories, driver=driver, wait=wait
     )
 
@@ -135,7 +205,7 @@ def run_scraper():
 
     print("Products Loaded \n\n\n")
     print("====================================")
-    print(products)
+    print(products_names_with_url)
     print("====================================")
 
     # save them here once db is ready
